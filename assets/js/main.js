aaac2cba1c215514588b151708ae7b702d90a09d
abdallah279 <abdallahzeko8@gmail.com>
2023-05-25 01:01:06 +0300

    new3

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ $(window).on("load", function () {
 });
 
 // Active Link
-$(".navbar-m .links a").each(function () {
+$(".navbar-m .links .link-ac").each(function () {
     if (window.location.href.includes($(this).attr("href"))) {
       $(this).addClass("active");
     }
@@ -19,9 +19,26 @@ window.addEventListener('scroll', function(){
   }
 });
 
+// Go To Up
+let upBtn = document.querySelector('.up-btn');
+
+window.addEventListener('scroll', function(){
+    // Go To UpBtn Show
+    if( window.scrollY > 80){
+        upBtn.classList.add('active')
+      }else{
+        upBtn.classList.remove('active')
+    }
+});
+
+// clicked to top
+upBtn.addEventListener('click', function(){
+    window.scrollTo(0,0);
+});
+
 // Show And Hide Search Navbar
 $('.search-open').on('click', function(){
-  $('.search-form').addClass('active');
+  $('.main-search').addClass('active');
   $('.overlay-m').fadeIn(600);
 });
 
@@ -36,24 +53,19 @@ $('.close').on('click', function(){
   $('.overlay-m').fadeOut(500);
 });
 
-// Toggle SideBar Filter
-$(".open-side-icon").on('click', function(){
-  $(this).next(".side-filter-cards").addClass('open-side');
-  $('.overlay-m').fadeIn(500);
-})
 
 // User Account filter
-$('.filter-open').on('click', function(){
-  $('.accordion-con').addClass('open');
+$('.open-filter').on('click', function(){
+  $('.flex-1').addClass('open');
   $('.overlay-m').fadeIn(500);
 })
 
 $('.overlay-m').on('click', function(){
-  $('.search-form').removeClass('active');
+  $('.main-search').removeClass('active');
   $('.links').removeClass('active');
   $(".side-filter-cards").removeClass('open-side');
   $('.side-links-con').removeClass('open');
-  $('.accordion-con').removeClass('open');
+  $('.flex-1').removeClass('open');
   $(this).fadeOut(500);
 });
 
@@ -63,30 +75,6 @@ $(".dropdown-menu").click(function(e){
     e.stopPropagation();
 });
 
-
-// PassWord Show In Setting Page
-const iconsPassSet = document.querySelectorAll('.pass-icon');
-
-if(iconsPassSet){
-  iconsPassSet.forEach((ic) =>{
-    ic.addEventListener('click', function(){
-      let input = ic.parentElement.querySelector('.input-me');
-      showPassword(input, ic);
-    });
-  });
-}
-
-// Function To Show And Hide Password
-function showPassword(input, icon){
-
-  if(input.type == 'password'){
-    input.setAttribute('type', 'text');
-    icon.innerHTML = `<i class="fa-regular fa-eye"></i>`;
-  } else{
-    input.setAttribute('type', 'password');
-    icon.innerHTML = `<i class="fa-regular fa-eye-slash"></i>`;
-  }
-}
 
 let isRtl = $('html[lang="ar"]').length > 0;
 
@@ -109,119 +97,4 @@ $(".select-filter").select2({
   dir: isRtl ? "rtl" : "ltr",
   minimumResultsForSearch: Infinity,
   theme: "filter-m"
-});
-
-
-
-// Code Modal
-$(document).ready(function(){
-  let codes = document.querySelectorAll(".code");
-
-  $(".code-container .code").first().focus();
-  codes.forEach((code, idx) => {
-    code.addEventListener("keydown", (e) => {
-      if (e.key >= 0 && e.key <= 9) {
-        codes[idx].value = "";
-        if([idx + 1] < codes.length){
-            setTimeout(() => codes[idx + 1].focus(), 10);
-        }
-      } else if (e.key === "Backspace") {
-        setTimeout(() => codes[idx - 1].focus(), 10);
-      }
-    });
-  });
-
-});
-
-
-// Select All CheckBoxs
-$( 'input[type="checkbox"]' ).click( function () {
-  if($(this).is($('.check-all'))){
-      var dataCheck = $(this).attr("data-check");
-      $(`.${dataCheck} input[type="checkbox"]` ).prop('checked', this.checked)
-  } else{
-
-    if(this.closest('.check-boxs')){
-      this.closest('.check-boxs').querySelector('.check-all').checked = false;
-    }
-  }
-
-  filterLoader(this)
-});
-
-$('.radio-box').click( function () {
-  filterLoader(this)
-});
-
-// Filter Loader
-function filterLoader(el){
-  $(el).parents('.side-filter-cards').find('.load').fadeIn();
-  $(el).parents('.side-filter-cards').find('.load-bg').fadeIn(200);
-  $(el).parents('.account-section').find('.load').fadeIn();
-  $(el).parents('.account-section').find('.load-bg').fadeIn(200);
-  
-  // setTimeout(() =>{
-  //   $(el).parents('.side-filter-cards').find('.load').fadeOut(200);
-  //   $(el).parents('.side-filter-cards').find('.load-bg').fadeOut();
-  //   $(el).parents('.account-section').find('.load').fadeOut(200);
-  //   $(el).parents('.account-section').find('.load-bg').fadeOut();
-  // }, 700)
-}
-
-// verticale Or linear
-$(document).on("change", ".select-filter", function () {
-  
-  if(this.options[this.selectedIndex].getAttribute('data-filter') == 'verticale'){
-      $(this).parents('.filter-container').find('.cards-container').addClass('verticale');
-      sessionStorage.setItem("filter", JSON.stringify('verticale'));
-    } else{
-      $(this).parents('.filter-container').find('.cards-container').removeClass('verticale');
-      sessionStorage.setItem("filter", JSON.stringify('linear'));
-  }
-
-});
-
-let dataFilter = sessionStorage.getItem("filter");
-let mySelect = document.querySelector('.select-filter');
-
-
-$('a').on('click', function(){
-  sessionStorage.setItem("filter", JSON.stringify('linear'));
-})
-
-$('.search-form').on('submit', function(){
-  sessionStorage.setItem("filter", JSON.stringify('linear'));
-})
-
-// Get Value From Local Storage
-if(dataFilter){
-    filterClass = JSON.parse(dataFilter);
-
-    $('.select-filter').val(filterClass)
-    $('.select-filter').trigger('change')
-
-    $('.filter-container').find('.cards-container').addClass(filterClass);
-    
-  } else{
-    $('.filter-container').find('.cards-container').removeClass('verticale');
-  
-}
-
-
-// Input Number
-$(document).ready(function() {
-  $('.minus').click(function () {
-    var $input = $(this).parent().find('input');
-    var count = parseInt($input.val()) - 1;
-    count = count < 1 ? 1 : count;
-    $input.val(count);
-    $input.change();
-    return false;
-  });
-  $('.plus').click(function () {
-    var $input = $(this).parent().find('input');
-    $input.val(parseInt($input.val()) + 1);
-    $input.change();
-    return false;
-  });
 });
